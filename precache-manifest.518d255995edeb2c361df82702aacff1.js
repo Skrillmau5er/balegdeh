f456cf008dadeb3d58e86472e3ef310b2b9de2e2
Alec Ostheimer <alecostheimer94@gmail.com>
2019-08-26 09:05:26 -0600

    Updates

---
+++ precache-manifest.518d255995edeb2c361df82702aacff1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d39ac5375be507ae2489f36d6733957",
+    "url": "/balegdeh/index.html"
+  },
+  {
+    "revision": "84f2f10d2da4ee0e7c6c",
+    "url": "/balegdeh/static/css/main.7669efd6.chunk.css"
+  },
+  {
+    "revision": "930f58ca72a520de828d",
+    "url": "/balegdeh/static/js/2.70fbeaf4.chunk.js"
+  },
+  {
+    "revision": "84f2f10d2da4ee0e7c6c",
+    "url": "/balegdeh/static/js/main.be6aaa45.chunk.js"
+  },
+  {
+    "revision": "c1774a443d50b7ec27a1",
+    "url": "/balegdeh/static/js/runtime~main.2e9d377e.js"
+  },
+  {
+    "revision": "46b0e4dd379329fc16ec9474c9dde24a",
+    "url": "/balegdeh/static/media/balegdeh_face.46b0e4dd.png"
+  },
+  {
+    "revision": "30eacd8061220961f80c5218f304ea1e",
+    "url": "/balegdeh/static/media/jamaica.30eacd80.m4a"
+  }
+]);
